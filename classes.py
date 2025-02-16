a7493a77e8bee6a38a472cebf1713f6ae6f48a6d
Rob Barton <robbybarton3@gmail.com>
2025-02-16 00:40:17 -0600

    Added non-cleric class skeletons

--- classes.py
+++ classes.py
@@ -1,24 +1,82 @@
-from stat_gambling import stat_array
+from stat_gambling import stat_array, normalized_number_str
 
-class adnd_class():
+class adnd_class:
     def __init__(self, stats:stat_array):
         self.stats:stat_array=stats
         self.level:int=1
+        self.abilities=""
+        self.name="class"
     
     def __str__(self)->str:
-        return "Str, Int, Wis, Dex, Con, Chr\n"+str(self.stats)
-
+        stat_line:str="Str, Int, Wis, Dex, Con, Chr\n"+str(self.stats)
+        class_abilities:str=self.name+" abilities"+"/n"+self.abilities
+        return stat_line+"/n/n"+class_abilities
 
 class cleric(adnd_class):
 
+    name="Cleric"
+
     def __init__(self, stats:stat_array):
-        super.__init__(stats)
-        self.abilities="""
-            - Turn undead (PHB 20)
-            - 2 weapon proficiencies (else -2 penalty)
-            - One 1st lvl spell\n
-        """
-    
-    def cleric_bonus():
+
+        super().__init__(stats)
+
+        self.abilities="""- Turn undead (PHB 20)\n- 2 weapon proficiencies (else -2 penalty)\n- One 1st lvl spell\n"""
+
+        self.name="Cleric"
+
         if self.stats.WIS > 15:
-            self.abilities+="+10% XP gain"
+            self.abilities+="- +10% XP gain\n"
+
+class Druid(adnd_class):
+    def __init__(self, stats:stat_array):
+        super().__init__(stats)
+
+        self.name="Druid"
+
+class Fighter(adnd_class):
+    def __init__(self, stats:stat_array):
+        super().__init__(stats)
+
+        self.name="Fighter"
+
+class Paladin(adnd_class):
+    def __init__(self, stats:stat_array):
+        super().__init__(stats)
+
+        self.name="Paladin"
+
+class Ranger(adnd_class):
+    def __init__(self, stats:stat_array):
+        super().__init__(stats)
+
+        self.name="Ranger"
+
+class Magic_user(adnd_class):
+    def __init__(self, stats:stat_array):
+        super().__init__(stats)
+
+        self.name="Magic-User"
+
+class Illusionist(adnd_class):
+    def __init__(self, stats:stat_array):
+        super().__init__(stats)
+
+        self.name="Illusionist"
+
+class Thief(adnd_class):
+    def __init__(self, stats:stat_array):
+        super().__init__(stats)
+
+        self.name="Thief"
+
+class Assassin(adnd_class):
+    def __init__(self, stats:stat_array):
+        super().__init__(stats)
+
+        self.name="Assassin"
+
+class Monk(adnd_class):
+    def __init__(self, stats:stat_array):
+        super().__init__(stats)
+
+        self.name="Monk"
